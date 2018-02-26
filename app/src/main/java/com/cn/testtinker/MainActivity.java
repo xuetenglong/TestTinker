ecfa0f384ce7db3f3d802629725b2ee776565696
xuetenglong <xuetenglongyeah@163.com>
2018-02-26 17:03:07 +0800

    add

--- app/src/main/java/com/cn/testtinker/MainActivity.java
+++ app/src/main/java/com/cn/testtinker/MainActivity.java
@@ -1,13 +1,30 @@
 package com.cn.testtinker;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.text);
+        textView.setText("12312123");
+        textView.setText("hello world！");
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"dianjia",Toast.LENGTH_SHORT).show();
+                TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+            }
+        });
     }
 }
